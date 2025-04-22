eeba78d8d850315793f480c3587af5f13ebd10cb
Ricky513125 <lilingyu513125@163.com>
2025-04-22 19:05:52 +0800

    check dimension

--- embedding_layer.py
+++ embedding_layer.py
@@ -87,11 +87,11 @@ class RecommenderModel(nn.Module):
         )
 
     def forward(self, user_data, item_data):
-        print('user_data', user_data)
-        print('shape', user_data.shape)
+        # print('user_data', user_data)
+        # print('shape', user_data.shape)
         print('user_data.keys()', user_data.keys())
         # print('item_data', item_data)
-        assert 1 ==0
+        # assert 1 ==0
         # job_id 验证
         # 在模型forward开头添加
         # print("job_id取值范围：", user_data['job_id'].min(), user_data['job_id'].max())
@@ -108,7 +108,9 @@ class RecommenderModel(nn.Module):
         # print('current_dynamic_pool', self.dynamic_pool.weight.shape)
 
         # 用户特征处理
+        print(f"user_id嵌入层尺寸: {self.user_id_emb.num_embeddings}")
 
+        print(f"use_data.shape: {user_data['user_id'].shape}")
         print('current_user_id', user_data['user_id'])
         u_id = self.user_id_emb(user_data['user_id'])
         print('current_gender', user_data['gender_id'])
